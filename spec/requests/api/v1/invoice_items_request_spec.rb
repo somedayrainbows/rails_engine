827155924a087b5e7608202984f1fdf3f76e99ab
Marisa Burton <marisamburton@gmail.com>
2017-05-02 15:47:50 -0600

    adds more testing and controllers

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -24,4 +24,51 @@ describe 'Items API' do
     expect(response).to be_success
     expect(invoice_item["id"]).to eq(id)
   end
+
+  it "can find a invoice item by name" do
+    name = create(:invoice_item).name
+
+    get "/api/v1/invoice_items/find?name=#{name}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["name"]).to eq(name)
+  end
+
+  it "can find an invoice-item by creation date" do
+    created_invoice_item = create(:invoice_item, created_at: "2017-05-02T03:04:05.000Z")
+
+    get "/api/v1/invoice_items/find?created_at=#{created_invoice_item.created_at}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["name"]).to eq(created_invoice_item.name)
+  end
+
+  it "can find an invoice-item by updated date" do
+    updated_invoice_item = create(:invoice_item, updated_at: "2017-05-02T03:04:05.000Z")
+
+    get "/api/v1/invoice_items/find?updated_at=#{updated_invoice_item.updated_at}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["name"]).to eq(updated_invoice_item.name)
+  end
+
+  it "can find all invoice-items by name" do
+    invoice_item1 = InvoiceItem.create(name: "glove")
+    invoice_item2 = InvoiceItem.create(name: "ball")
+    invoice_item3 = InvoiceItem.create(name: "bat")
+
+    get "/api/v1/invoice_items/find_all?name=#{invoice_item1.name}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item[0]["name"]).to eq("glove")
+
+  end
 end
