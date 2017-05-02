b24a56641334ff39c4809dd543666435a12808fd
Cody <codyborders@gmail.com>
2017-05-02 14:12:06 -0600

    Adds tests to find all merchants by created/updated dates

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -82,4 +82,30 @@ describe "Merchants API" do
     
     expect(merchants[0]["name"]).to eq("Merchant1")
   end
+
+  it "can find all merchants by created date" do
+     merchant1 = Merchant.create(created_at: "2012-05-02T03:04:05.000Z")
+     merchant2 = Merchant.create(created_at: "2017-05-02T03:04:05.000Z")
+     merchant3 = Merchant.create(created_at: "2017-05-02T03:04:06.000Z")
+
+     get "/api/v1/merchants/find_all?created_at=#{merchant1.created_at}"
+
+     merchants = JSON.parse(response.body)
+
+     expect(response).to be_success
+     expect(merchants[0]["name"]).to eq(merchant1.name)
+   end
+
+    it "can find all merchants by updated date" do
+      merchant1 = Merchant.create(updated_at: "2017-05-02T03:04:05.000Z")
+      merchant2 = Merchant.create(updated_at: "2017-05-02T03:04:05.000Z")
+      merchant3 = Merchant.create(updated_at: "2017-05-17T03:04:06.000Z")
+
+      get "/api/v1/merchants/find_all?updated_at=#{merchant1.updated_at}"
+
+      merchants = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchants[0]["name"]).to eq(merchant1.name)
+    end
 end
