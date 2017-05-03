f77fcce800a8e8438617eb4aa0daefa2f74ffb29
Cody <codyborders@gmail.com>
2017-05-03 07:26:59 -0600

    Adds spec to find transaction by ID

--- spec/requests/api/v1/transaction_request_spec.rb
+++ spec/requests/api/v1/transaction_request_spec.rb
@@ -13,4 +13,16 @@ describe "Transactions API" do
     expect(transactions.count).to eq(3)
 
   end
+
+  it "can find transactions by id" do
+    id = create(:transaction).id
+
+    get "/api/v1/transactions/#{id}"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+
+    expect(transaction["id"]).to eq(id)
+  end
 end
