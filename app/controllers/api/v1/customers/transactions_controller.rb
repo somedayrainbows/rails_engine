eba0f6542d3024d1350b53dccbe679a38eca7104
Cody Borders <codyborders@gmail.com>
2017-05-04 11:13:47 -0600

    Fixes line spacing

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::Customers::TransactionsController < ApplicationController
-
+  
 	def index
 		customer = Customer.find(params[:id])
 		render json: customer.transactions
 	end
-
 end
