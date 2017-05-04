bee72d9ab23bfb1b05461861480a12b643c2aeb2
Cody <codyborders@gmail.com>
2017-05-04 11:58:59 -0600

    fixes migration errors Transactions were added to invoices instead of invoices being added to transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503204826) do
+ActiveRecord::Schema.define(version: 20170503232747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,10 +59,12 @@ ActiveRecord::Schema.define(version: 20170503204826) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.text    "credit_card_number"
-    t.date    "credit_card_expiration_date"
-    t.string  "result"
-    t.integer "invoice_id"
+    t.text     "credit_card_number"
+    t.date     "credit_card_expiration_date"
+    t.string   "result"
+    t.integer  "invoice_id"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id", using: :btree
   end
 
